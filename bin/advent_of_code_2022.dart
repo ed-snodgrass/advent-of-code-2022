beef0829d6558c0856d24116a7ec18367c3cf10b
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2022-12-01 06:44:11 -0600

    day1 puzzle 1 and 2 with some post-submission refactoring

--- bin/advent_of_code_2022.dart
+++ bin/advent_of_code_2022.dart
@@ -1,3 +1,6 @@
+import 'package:advent_of_code_2022/day_1.dart' as advent_of_code_2022;
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  advent_of_code_2022.day1Puzzle1();
+  advent_of_code_2022.day1Puzzle2();
 }
---
+++ lib/day_1.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+import 'dart:async';
+
+Future<List<String>> parseInput() async {
+  final file = File('inputs/day_1_puzzle_1.txt');
+  return file.readAsLines();
+}
+
+List<int> groupElvesByCalories(List<String> input) {
+  var elves = <int>[];
+  int currentElf = 0;
+  elves.add(0);
+  for (var line in input) {
+    if (line.isEmpty) {
+      currentElf++;
+      elves.add(0);
+    } else {
+      elves[currentElf] += int.parse(line);
+    }
+  }
+  return elves;
+}
+
+void day1Puzzle1() async {
+  try {
+    var lines = await parseInput();
+    var elves = groupElvesByCalories(lines);
+    elves.sort((a, b) => b.compareTo(a));
+    print('Elf with the most calories: ${elves[0]}');
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+
+
+void day1Puzzle2() async {
+  try {
+    var lines = await parseInput();
+    var elves = groupElvesByCalories(lines);
+    elves.sort((a, b) => b.compareTo(a));
+    print('Total of top 3 elves: ${elves[0] + elves[1] + elves[2]}');
+  } catch (e) {
+    print('Error: $e');
+  }
+}
