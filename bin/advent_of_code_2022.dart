93b1b7be42cae805bf061bda5d09dc9edb42683b
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2022-12-01 23:23:06 -0600

    day 2 puzzle solutions

--- bin/advent_of_code_2022.dart
+++ bin/advent_of_code_2022.dart
@@ -1,6 +1,9 @@
 import 'package:advent_of_code_2022/day_1.dart' as advent_of_code_2022;
+import 'package:advent_of_code_2022/day_2.dart' as advent_of_code_2022;
 
 void main(List<String> arguments) {
-  advent_of_code_2022.day1Puzzle1();
-  advent_of_code_2022.day1Puzzle2();
+  // advent_of_code_2022.day1Puzzle1();
+  // advent_of_code_2022.day1Puzzle2();
+  // advent_of_code_2022.day2Puzzle1();
+  advent_of_code_2022.day2Puzzle2();
 }
---
+++ lib/day_2.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+import 'dart:async';
+
+Future<List<String>> parseInput() async {
+  final file = File('inputs/day_2_puzzle_1.txt');
+  return file.readAsLines();
+}
+
+void day2Puzzle1() async {
+  try {
+    var lines = await parseInput();
+    var score = 0;
+
+    for (var line in lines) {
+      var choices = line.split(' ');
+      if (choices[0] == 'A') {
+        if (choices[1] == 'X') {
+          score += 1;
+          score += 3;
+        } else if(choices[1] == 'Y') {
+          score += 2;
+          score += 6;
+        } else if(choices[1] == 'Z') {
+          score += 3;
+          score += 0;
+        }
+      } else if (choices[0] == 'B') {
+        if (choices[1] == 'X') {
+          score += 1;
+          score += 0;
+        } else if(choices[1] == 'Y') {
+          score += 2;
+          score += 3;
+        } else if(choices[1] == 'Z') {
+          score += 3;
+          score += 6;
+        }
+      } else if (choices[0] == 'C') {
+        if (choices[1] == 'X') {
+          score += 1;
+          score += 6;
+        } else if(choices[1] == 'Y') {
+          score += 2;
+          score += 0;
+        } else if(choices[1] == 'Z') {
+          score += 3;
+          score += 3;
+        }
+      }
+      print(score);
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+
+void day2Puzzle2() async {
+  try {
+    var lines = await parseInput();
+    var score = 0;
+
+    for (var line in lines) {
+      var choices = line.split(' ');
+      if (choices[0] == 'A') {
+        if (choices[1] == 'X') {
+          score += 3;
+          score += 0;
+        } else if(choices[1] == 'Y') {
+          score += 1;
+          score += 3;
+        } else if(choices[1] == 'Z') {
+          score += 2;
+          score += 6;
+        }
+      } else if (choices[0] == 'B') {
+        if (choices[1] == 'X') {
+          score += 1;
+          score += 0;
+        } else if(choices[1] == 'Y') {
+          score += 2;
+          score += 3;
+        } else if(choices[1] == 'Z') {
+          score += 3;
+          score += 6;
+        }
+      } else if (choices[0] == 'C') {
+        if (choices[1] == 'X') {
+          score += 0;
+          score += 2;
+        } else if(choices[1] == 'Y') {
+          score += 3;
+          score += 3;
+        } else if(choices[1] == 'Z') {
+          score += 1;
+          score += 6;
+        }
+      }
+      print(score);
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+}
