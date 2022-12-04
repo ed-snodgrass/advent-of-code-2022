1c40f89a1cddeceb103bc621aac53970a5be887f
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2022-12-04 09:09:35 -0600

    refactored day4 code, less code, possibly not as clean

--- lib/day_4.dart
+++ lib/day_4.dart
@@ -1,53 +1,34 @@
 import 'dart:io';
 import 'dart:async';
 
-Future<List<String>> parseInput() async {
+List<List<int>> getElfSectionRange(String lineInput) {
+  var elfPair = lineInput.split(',');
+  return [elfPair[0].split('-').map((e) => int.parse(e)).toList(), elfPair[1].split('-').map((e) => int.parse(e)).toList()];
+}
+
+Future<Iterable<List<List<int>>>> parseInput() async {
   final file = File('inputs/day_4_puzzle.txt');
-  return file.readAsLines();
+  final lines = await file.readAsLines();
+  return lines.map((line) => getElfSectionRange(line));
 }
 
-List<int> extrapolateRange(range) {
-  List<int> extrapolatedRange = [];
-  int beginning = int.parse(range[0]);
-  int ending = int.parse(range[1]);
-  if (beginning == ending) {
-    extrapolatedRange.add(beginning);
-    return extrapolatedRange;
-  }
-  for (int i = beginning; i <= ending; i++) {
-    extrapolatedRange.add(i);
-  }
-
-  return extrapolatedRange;
+bool fullyContains(elfSectionList) {
+  var range1 = elfSectionList[0];
+  var range2 = elfSectionList[1];
+  return ((range1[0] >= range2[0] && range1[1] <= range2[1]) || (range2[0] >= range1[0] && range2[1] <= range1[1]));
 }
 
-bool fullyContains(List<int> range1, List<int> range2) {
-  return range1.every((range1Value) => range2.contains(range1Value)) || range2.every((range2Value) => range1.contains(range2Value));
-}
-
-bool hasFullyContainedRange(elfPairSectionList) {
-  var elfPair = elfPairSectionList.split(',');
-  var firstElfRange = extrapolateRange(elfPair[0].split('-'));
-  var secondElfRange = extrapolateRange(elfPair[1].split('-'));
-  return fullyContains(firstElfRange, secondElfRange);
-}
-
-bool containsAny(List<int> range1, List<int> range2) {
-  return range1.any((range1Value) => range2.contains(range1Value)) || range2.any((range2Value) => range1.contains(range2Value));
-}
-
-bool hasAnyOverlap(elfPairSectionList) {
-  var elfPair = elfPairSectionList.split(',');
-  var firstElfRange = extrapolateRange(elfPair[0].split('-'));
-  var secondElfRange = extrapolateRange(elfPair[1].split('-'));
-  return containsAny(firstElfRange, secondElfRange);
+bool containsAny(elfSectionList) {
+  var range1 = elfSectionList[0];
+  var range2 = elfSectionList[1];
+  return ((range1[0] >= range2[0] && range1[0] <= range2[1]) || (range2[0] >= range1[0] && range2[0] <= range1[1]));
 }
 
 Future<void> puzzle1() async {
   try {
-    var lines = await parseInput();
-    int fullyContainedCount = lines.where(hasFullyContainedRange).length;
-    print('Day4 Puzzle1 fully contained count: $fullyContainedCount');
+    var elfSectionRange = await parseInput();
+    int fullyContainedCount = elfSectionRange.where(fullyContains).length;
+    print('Day4 Puzzle1 fully contained count2: $fullyContainedCount');
   } catch (e) {
     print('Error: $e');
   }
@@ -55,8 +36,8 @@ Future<void> puzzle1() async {
 
 Future<void> puzzle2() async {
   try {
-    var lines = await parseInput();
-    int overlapCount = lines.where(hasAnyOverlap).length;
+    var elfSectionRange = await parseInput();
+    int overlapCount = elfSectionRange.where(containsAny).length;
     print('Day4 Puzzle2 overlap count: $overlapCount');
   } catch (e) {
     print('Error: $e');
